47a3c0e5e0d5bf895f81c676505175229f39e21e
Sujeet Kumar <sujeet@tolexo.com>
2017-03-18 09:54:02 +0530

    added new feature to assign todo task

--- includes/config.php
+++ includes/config.php
@@ -17,10 +17,16 @@ define('DB_DATABASE', 'todo'); 			// Database name
 
 define('IST_TIME_ADJUSTMENT', '+5 hours +30 minutes');
 
+define('SLACK_INCOMMING_WEBHOOK2', 'xx');
 /*
  *   Slack configurations
  */
-define('SLACK_TOKEN', 'xxx');			// Slack Token for Verification   (or ip like 192.168.0.1)
-define('SLACK_INCOMMING_WEBHOOK', 'yyy');			// Slack Token for Verification   (or ip like 192.168.0.1)
+define('SLACK_TOKEN', 'xx');			// Slack Token for Verification   (or ip like 192.168.0.1)
+define('SLACK_INCOMMING_WEBHOOK', 'xx');
+
+define('SLACK_WEB_API_URL', 'xx');
+define('SLACK_URL_USER_LIST_METHOD', 'users.list');
+define('SLACK_URL_CHANNEL_LIST_METHOD', 'channels.list');
+define('SLACK_OAUTH_TOKEN', 'xx');
 
 define('MAX_TODOS', 200);
